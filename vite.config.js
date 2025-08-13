174e835da48018ef7504133b94408a960198a31e
Muhammad Ridho <55222870+ridhorambu93@users.noreply.github.com>
2025-08-14 00:29:47 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: "/ridhorambu93-portfolio",
-  base: "",
-})
+  base: "/ridhorambu93-portfolio/",
+});
