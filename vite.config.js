5922a7ee9d6e37c86657c902172e677c67443ed1
Muhammad Ridho <55222870+ridhorambu93@users.noreply.github.com>
2025-08-14 00:31:32 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://github.com/ridhorambu93/ridhorambu93.github.io",
+  base: "https://ridhorambu93.github.io/",
 });
