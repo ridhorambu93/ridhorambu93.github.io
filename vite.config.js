71b5f451bbb878f15587a63dbbfd709e9b669b58
ridhorambu93 <ridhodavybones@gmail.com>
2025-08-14 00:50:51 +0700

    update -v1

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-//base: "https://ridhorambu93.github.io/",
-  base: "/ridhorambu93.github.io/",
-});
+  plugins: [react()],
+})
